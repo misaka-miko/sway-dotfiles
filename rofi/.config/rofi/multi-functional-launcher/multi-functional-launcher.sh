bc2f8cbd0efd7e6dc5402001eb5b788047ee7a21
misaka <smileMisaka@outlook.com>
2025-06-17 23:03:51 +0800

    break change:using stow to manage my dotfiles

---
+++ rofi/.config/rofi/multi-functional-launcher/multi-functional-launcher.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+dir="$HOME/.config/rofi/multi-functional-launcher"
+
+rofi -show drun -theme ${dir}/style.rasi
---
+++ rofi/.config/rofi/multi-functional-launcher/window-switcher.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+dir="$HOME/.config/rofi/multi-functional-launcher"
+
+rofi -show window -theme ${dir}/style.rasi
